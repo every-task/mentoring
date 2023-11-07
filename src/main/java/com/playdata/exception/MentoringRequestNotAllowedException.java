13ee3313fc46459f8054e5c2e7e6e75c10bdd901
energysheep <yks7560@gmail.com>
2023-11-07 11:52:54 +0900

    feat : 멘토링 요청 있을경우 예외처리

---
+++ src/main/java/com/playdata/exception/MentoringRequestNotAllowedException.java
@@ -0,0 +1,7 @@
+package com.playdata.exception;
+
+public class MentoringRequestNotAllowedException extends RuntimeException{
+    public MentoringRequestNotAllowedException(String message) {
+        super(message);
+    }
+}
