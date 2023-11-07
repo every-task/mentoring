b888116fda85688e3493de7524b239f4b7d1bade
energysheep <yks7560@gmail.com>
2023-11-07 11:49:35 +0900

    chore : 변수명 수정

--- src/main/java/com/playdata/domain/mentoring/response/MentoringRequest.java
+++ src/main/java/com/playdata/domain/mentoring/response/MentoringRequest.java
@@ -6,5 +6,6 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class MentoringRequest {
-    private String toMemberId;
+    private String mentorId;
+    private String menteeId;
 }
