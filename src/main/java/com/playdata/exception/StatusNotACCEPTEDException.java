c96841f0b2407f3321e287a874b7dae035f57d4f
energysheep <yks7560@gmail.com>
2023-11-08 16:37:15 +0900

    feat : Mentoring status가 ACCEPTED일 경우에만 쪽지 가능

---
+++ src/main/java/com/playdata/exception/StatusNotACCEPTEDException.java
@@ -0,0 +1,7 @@
+package com.playdata.exception;
+
+public class StatusNotACCEPTEDException extends RuntimeException{
+    public StatusNotACCEPTEDException(String message) {
+        super(message);
+    }
+}
